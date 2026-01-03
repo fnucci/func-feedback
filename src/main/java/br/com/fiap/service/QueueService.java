aa7af8514bbae4692dc86460cc3e8e14f46b04d1
Felipe Nucci <flpnucci@gmail.com>
2026-01-03 17:12:08 -0300

    feature: Mudança nos lambdas dos métodos de utilização das Messages do SQS que estavam prejudicando o teste unitario

--- src/main/java/br/com/fiap/service/QueueService.java
+++ src/main/java/br/com/fiap/service/QueueService.java
@@ -4,7 +4,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
 import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 import java.util.List;
 
@@ -13,7 +16,7 @@ public class QueueService {
 
     private SqsClient sqsClient;
 
-    @ConfigProperty(name="sqs.queue.url")
+    @ConfigProperty(name = "sqs.queue.url")
     private String queueUrl;
 
     public QueueService() {
@@ -23,23 +26,30 @@ public class QueueService {
     }
 
     public void sendMessage(String messageBody) {
-        sqsClient.sendMessage(m -> m
+        SendMessageRequest request = SendMessageRequest.builder()
                 .queueUrl(queueUrl)
                 .messageBody(messageBody)
-        );
+                .build();
+        sqsClient.sendMessage(request);
+
     }
 
     public Message recieveMessage() {
-        List<Message> messages = sqsClient.receiveMessage(r -> r
+        ReceiveMessageRequest request = ReceiveMessageRequest.builder()
                 .queueUrl(queueUrl)
                 .maxNumberOfMessages(1)
+                .build();
+        List<Message> messages = sqsClient.receiveMessage(request
         ).messages();
 
         if (messages.isEmpty()) {
             return null;
         }
-        sqsClient.deleteMessage(d -> d.queueUrl(queueUrl)
-                .receiptHandle(messages.get(0).receiptHandle()));
+        DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .receiptHandle(messages.get(0).receiptHandle())
+                .build();
+        sqsClient.deleteMessage(deleteMessageRequest);
         return messages.get(0);
     }
 }
