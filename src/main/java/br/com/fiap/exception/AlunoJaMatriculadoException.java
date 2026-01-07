656e89e521c5b12437c3d43da31b24f816fda5f7
Felipe Nucci <flpnucci@gmail.com>
2026-01-07 15:51:00 -0300

    chore: remoção das classes nao mais utilizadas

--- src/main/java/br/com/fiap/exception/AlunoJaMatriculadoException.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.exception;
-
-public class AlunoJaMatriculadoException extends RuntimeException {
-    public AlunoJaMatriculadoException(String msg) {
-        super(msg);
-    }
-}
