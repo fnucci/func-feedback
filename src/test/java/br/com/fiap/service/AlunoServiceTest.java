4dd8c40f7be63de84b80f556f6ce9486aa2052ee
Felipe Nucci <flpnucci@gmail.com>
2026-01-07 15:48:32 -0300

    test: Mais testes para terminar esse projeto

--- src/test/java/br/com/fiap/service/AlunoServiceTest.java
+++ src/test/java/br/com/fiap/service/AlunoServiceTest.java
@@ -1,4 +1,46 @@
 package br.com.fiap.service;
 
+import br.com.fiap.exception.AlunoNotFoundException;
+import br.com.fiap.persistence.entity.Aluno;
+import br.com.fiap.persistence.repository.AlunoRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
 public class AlunoServiceTest {
+
+    @Test
+    void findAlunoById() {
+        // arrange
+        AlunoRepository mockAlunoRepository = Mockito.mock(AlunoRepository.class);
+
+        Aluno aluno = Mockito.mock(Aluno.class);
+
+        when(mockAlunoRepository.findById(1L)).thenReturn(aluno);
+        when(aluno.getName()).thenReturn("Test Student");
+
+        var response = mockAlunoRepository.findById(1l);
+
+        // assert
+        assertNotNull(response);
+        verify(mockAlunoRepository, times(1)).findById(anyLong());
+    }
+
+    @Test
+    void findAlunoByIdThrowsException() {
+        // arrange
+        AlunoRepository mockAlunoRepository = Mockito.mock(AlunoRepository.class);
+
+        when(mockAlunoRepository.findById(99L)).thenThrow(AlunoNotFoundException.class);
+
+        // assert
+        assertThrows(AlunoNotFoundException.class, () -> {
+            mockAlunoRepository.findById(99l);;
+        });
+
+        verify(mockAlunoRepository, times(1)).findById(anyLong());
+    }
 }
--- src/test/java/br/com/fiap/service/CursoServiceTest.java
+++ src/test/java/br/com/fiap/service/CursoServiceTest.java
@@ -1,4 +1,48 @@
 package br.com.fiap.service;
 
+import br.com.fiap.exception.CursoNotFoundException;
+import br.com.fiap.persistence.entity.Curso;
+import br.com.fiap.persistence.repository.CursoRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
 public class CursoServiceTest {
+
+    @Test
+    void findCursoById() {
+        // arrange
+        CursoRepository mockCursoRepository = Mockito.mock(CursoRepository.class);
+
+        Curso curso = Mockito.mock(Curso.class);
+
+        when(mockCursoRepository.findById(1L)).thenReturn(curso);
+        when(curso.getName()).thenReturn("Test Course");
+
+        var response = mockCursoRepository.findById(1l);
+
+        // assert
+        assertNotNull(response);
+        verify(mockCursoRepository, times(1)).findById(anyLong());
+    }
+
+    @Test
+    void findCursoByIdThrowsException() {
+        // arrange
+        CursoRepository mockCursoRepository = Mockito.mock(CursoRepository.class);
+
+        when(mockCursoRepository.findById(99L)).thenThrow(CursoNotFoundException.class);
+
+        // assert
+        assertThrows(CursoNotFoundException.class, () -> {
+            mockCursoRepository.findById(99l);
+            ;
+        });
+
+        verify(mockCursoRepository, times(1)).findById(anyLong());
+    }
 }
